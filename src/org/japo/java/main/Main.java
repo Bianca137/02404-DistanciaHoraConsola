7f251889411b914b35c1c08f4d56a374432e712c
CicloM <CicloM@192.168.2.137>
2019-10-20 18:36:32 +0200

    :tada: Commit Inicial

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -15,15 +15,74 @@
  */
 package org.japo.java.main;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author Bianca Antonela Glavan - biancaantonela.glavan.alum@iescamp.es
  */
 public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
     public static void main(String[] args) {
-        
-    
-        
+
+
+        Calendar cal = Calendar.getInstance();
+
+        try {
+
+            //Rótulo
+            System.out.println("Distancia entre Horas");
+            System.out.println("======================");
+
+            //Hora inicio introducida por el usuario
+            System.out.print("Hora inicio.......:");
+            int hEnt = SCN.nextInt();
+            SCN.nextLine();
+
+            System.out.print("Minuto inicio");
+            int mEnt = SCN.nextInt();
+            SCN.nextLine();
+
+            System.out.print("Segundo inicio");
+            int sEnt = SCN.nextInt();
+            SCN.nextLine();
+
+            //Hora actual
+            int hAct = cal.get(Calendar.HOUR_OF_DAY);
+            int mAct = cal.get(Calendar.MINUTE);
+            int sAct = cal.get(Calendar.SECOND);
+
+            //Tiempos
+            int tiempoEnt = hEnt * 3600 + mEnt * 60 + sEnt; //pasa todo a segundos porque es más preciso operar en java con segundos
+            int tiempoAct = hAct * 3600 + mAct * 60 + sAct;
+
+            //Segundos
+            int sDis = tiempoAct - tiempoEnt;
+
+            //Minutos
+            int mDis = sDis / 60;
+            sDis = sDis % 60;
+
+            //Horas
+            int hDis = mDis / 60;
+            mDis = mDis % 60;
+
+            //Salidas
+            System.out.printf("Hora actual.....:%02d:%02d:%02d%n", hAct, mAct, sAct);
+            System.out.printf("Hora inicio.....:%02d:%02d:%02d%n", hEnt, mEnt, sEnt);
+            System.out.printf("Tiempo clase.....:%02d:%02d:%02d%n", hDis, mDis, sDis);
+
+        } catch (Exception e) {
+            System.out.println("ERROR: Entrada incorrecta");
+        }
+
     }
-    
+
 }
